0af35d0c17861b69c853e22b222bb9fb0549f406
Suraj-Dal <dsurajdal10@gmail.com>
2022-05-31 15:36:33 +0530

    [Suraj]: Usecase 2 to add and display elements in linked list is added.

--- LinkedList/LinkedList/Program.cs
+++ LinkedList/LinkedList/Program.cs
@@ -1,5 +1,20 @@
 ﻿LinkedList.LinkedList list = new LinkedList.LinkedList();
-list.Add(56);
-list.Add(30);
-list.Add(70);
-list.Display();
+Console.WriteLine("LinkedList Operations:\n0.Exit\n2.Add\n3.Display\nEnter your choice:");
+int choice = Convert.ToInt32(Console.ReadLine());
+while (choice != 0 )
+{
+    switch (choice)
+    {
+        case 1:
+            Console.WriteLine("Enter data to add.");
+            int data = Convert.ToInt32(Console.ReadLine());
+            list.Add(data);
+            break;
+        case 2:
+            list.Display();
+            break;
+        default:
+            Console.WriteLine("Invalid option.");
+            break;
+    }
+}
